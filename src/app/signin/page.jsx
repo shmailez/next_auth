a9fbf8da8e00ee78d89083daa3563e94ed337df0
Anton Mamontov <a.mamontov@p-s.kz>
2024-07-10 23:56:42 +0500

    signin page use server

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,21 +1,7 @@
-'use client'
 import { GoogleButton } from "@/components/GooglrButton";
 import SignInForm from "@/components/SignInForm";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation"
 
 export default  function Signin() {
-
-    const session = useSession()
-    console.log(session)
-
-    const router = useRouter()
-    console.log(router)
-
-    console.log(session?.status)
-
-    // if (session?.status === 'authenticated') router.push('/profile')
-    
 
     return <>
     <div className="singpage">
