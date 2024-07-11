8a45cab2ae21961b6868741194142cfa9bd89a4c
Anton Mamontov <a.mamontov@p-s.kz>
2024-07-11 13:33:05 +0500

    suspence adding

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,14 +1,21 @@
 import { GoogleButton } from "@/components/GooglrButton";
 import SignInForm from "@/components/SignInForm";
+import { Suspense } from "react";
 
 export default  function Signin() {
     return <>
+    <Suspense fallback={<Loading />}>
     <div className="singpage">
         <h1>Sign In</h1>
         <GoogleButton/>
         <p>OR</p>
         <SignInForm/>
     </div>
+    </Suspense>
         
     </>
 }
+
+function Loading() {
+    return <h2>🌀 Loading...</h2>;
+  }
